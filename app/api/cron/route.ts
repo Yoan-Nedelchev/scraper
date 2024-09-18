b1aa0820798127bfe9868828690bc8cab91c6778
Yoan-Nedelchev <yoan.nedelchev@yahoo.com>
2024-09-18 16:42:06 +0300

    add route for testing cron job

---
+++ app/api/cron/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+// Handling GET requests
+export async function GET() {
+  console.log("Cron job or frontend request invoked this API!");
+
+  // You can add your cron job logic or any other logic here.
+  return NextResponse.json({ message: "Job successfully invoked!" });
+}
